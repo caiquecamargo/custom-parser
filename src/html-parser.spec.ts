da9f107ac56635bce1920c8c797f3e65cfd17860
caiquecamargo <caique.de.camargo@hotmail.com>
2024-03-28 18:54:02 -0300

    add html parser

---
+++ src/html-parser.spec.ts
@@ -0,0 +1,51 @@
+import { describe, expect, it } from "vitest";
+import { HTMLParserConfig, parseHtml } from "./html-parser";
+
+const config: HTMLParserConfig = {
+  marks: [
+    { tagName: "mark", replace: (element) => `{{${element.properties.dataProccess}}}` },
+    { tagName: "b", replace: (_, content) => `**${content}**` },
+    { tagName: "i", replace: (_, content) => `*${content}*` },
+    { tagName: "span", replace: (element, content) => `##[[${element.properties.style}]]${content}##` },
+  ],
+}
+
+describe('html-parser', () => {
+  it.each([
+    [
+      "",
+      ""
+    ],
+    [
+      `<mark contenteditable="false" class="bg-primary text-white p-1 rounded" data-proccess="personalYearText">Ano pessoal: texto</mark>`,
+      "{{personalYearText}}",
+    ],
+    [
+      "<b>bold text</b>",
+      "**bold text**"
+    ],
+    [
+      "<i>italic text</i>",
+      "*italic text*"
+    ],
+    [
+      "<b><i>red text</i></b>",
+      "***red text***"
+    ],
+    [
+      "<span style=\"color:#6f1f1f; and a wider content\">estabelecido </span>",
+      "##[[color:#6f1f1f; and a wider content]]estabelecido ##"
+    ],
+    [
+      "É o ano de <b>iniciar</b> novas coisas, onde <i>todo</i> estilo será <span style=\"color:#6f1f1f;\">estabelecido </span>pelo ciclo de nove anos. É a <i>hora de <b>ter</b> iniciativa e ser corajoso</i> e determinado. Se <b><i>quiser</i></b> sucesso e felicidade, a <span style=\"color:#6f1f1f;\">pessoa <i>precisa</i> ser <b>criativa</b>, segura, independente</span> e confiar na própria intuição. Deve tomar cuidado com a apatia e procrastinação, ou seja, deixar tudo para depois.",
+      "É o ano de **iniciar** novas coisas, onde *todo* estilo será ##[[color:#6f1f1f;]]estabelecido ##pelo ciclo de nove anos. É a *hora de **ter** iniciativa e ser corajoso* e determinado. Se ***quiser*** sucesso e felicidade, a ##[[color:#6f1f1f;]]pessoa *precisa* ser **criativa**, segura, independente## e confiar na própria intuição. Deve tomar cuidado com a apatia e procrastinação, ou seja, deixar tudo para depois."
+    ],
+    [
+      "É o ano de <b>iniciar</b> novas <b>coisas, onde <i>todo <b>novo</b> doido</i> estilo</b> será <span style=\"color:#6f1f1f;\">estabelecido </span>",
+      "É o ano de **iniciar** novas **coisas, onde *todo **novo** doido* estilo** será ##[[color:#6f1f1f;]]estabelecido ##"
+    ]
+  ])('should process input', async (html, expected) => {
+    const processed = parseHtml(html, config);
+    expect(processed).toEqual(expected);
+  })
+});
---
+++ src/html-parser.ts
@@ -0,0 +1,55 @@
+import { Root, Element, Comment } from "hast";
+import { rehype } from "rehype";
+import { VFile } from "vfile";
+import { ParserResponse } from "./parser";
+
+export interface HTMLMark {
+  tagName: string;
+  replace: (element: Element, content: string) => string;
+}
+
+export interface HTMLParserConfig {
+  marks: HTMLMark[];
+}
+
+// const unprocessElement = (element: ElementKey, input: string, node: Node) => {
+//   switch (element) {
+//     case "b":
+//       return `**${input}**`;
+//     case "i":
+//       return `*${input}*`;
+//     case "font":
+//       return `##[[${StyleParser.fromHTMLStyle(node.properties.style)}]]${input}##`;
+//     case "mark":
+//       return `{{${node.properties.dataProccess}}}`;
+//     default:
+//       return input;
+//   }
+// }
+
+const unrun = (node: Element | Comment, input: string, marks: HTMLMark[]) => {
+  if (node.type === "comment") return "";
+
+  return node.children.map((child) => {
+    if (child.type === "comment") return "";
+    if (child.type === "text") return child.value;
+
+    const mark = marks.find(mark => mark.tagName === child.tagName);
+
+    return mark?.replace(child, unrun(child, input, marks));
+  }).join("");
+}
+
+const plugin = (config?: HTMLParserConfig) => () => {
+  return (tree: Root, file: VFile) => {
+    const marks = config?.marks ?? [];
+    const [_, body] = (tree.children.shift() as Element).children;
+    file.data.parsed = unrun(body as Element | Comment, file.value as string, marks)
+  }
+}
+  
+export const parseHtml = (input: string, config?: HTMLParserConfig) => {
+  const tree = rehype().use(plugin(config)).processSync(input);
+
+  return tree.data.parsed as ParserResponse[];
+}
